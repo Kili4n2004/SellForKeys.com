abbab80ed46a928bf07e831ea05be589b5d4b0f7
Kili4n2004 <34780454+Kili4n2004@users.noreply.github.com>
2017-12-22 23:07:05 +0100

    First Upload
    The first upload of the website

---
+++ README.md
@@ -0,0 +1,53 @@
+# Web Based Trading Bot for CS:GO
+
+[![dependencies](https://img.shields.io/david/pepzwee/node-csgo-web-tradebot.svg)](https://github.com/pepzwee/node-csgo-web-tradebot)
+[![npm](https://img.shields.io/npm/l/express.svg?style=flat-square)](https://github.com/pepzwee/node-csgo-web-tradebot/blob/master/LICENSE)
+[![steam](https://img.shields.io/badge/steam-donate-green.svg?style=flat-square)](https://steamcommunity.com/tradeoffer/new/?partner=78261062&token=2_WUiltH)
+[![paypal](https://img.shields.io/badge/paypal-donate-yellow.svg)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=XKPQ3TWDYJ2Z6)
+
+### Do not add PEPZ on Steam. If you have a question or need help, look through "Issues" and create a new issue if you can't find an answer.
+
+This is a quickly coded website for people who want to see how these kind of websites work. Before you decide to use this script be aware, that this script, may have bugs.
+I also suggest looking through the code to see how it works, and if you spot an error make a pull request.
+
+Live preview:
+
+* [https://csg0.trade](https://csg0.trade)
+
+# API Usage
+
+### Prices
+
+The script gets its prices from SteamApis. If you don't feel comfortable with any of the API's this script uses, feel free to change them to your preferred one.
+
+1. SteamApis.com API for market tier items, using the `safe_price` values provided by it.
+
+<pre>* SteamApis provides high tier items as well.</pre>
+
+### Inventory
+
+To avoid Steam rate-limits all inventories are loaded through SteamApis.com inventory proxy. It utilizes thousands of proxies to maintain a high success rate.
+
+### SteamApis.com
+
+SteamApis is a paid service. You can get the API key at https://steamapis.com
+
+**You need to enable these endpoints in the Upgrade page:**
+
+- `market/items`
+- `steam/inventory`
+
+# Support and Development
+
+### Development
+
+I don't plan on adding any additional features. But feel free to add them yourself and make a pull request.
+
+### Support
+
+Keep in mind this project is free and open-source. Don't expect support to be good and fast.
+
+### Contributors
+
+Author: [PEPZ](https://pepzwee.com)
+You can view the [people who have helped with this project here.](https://github.com/pepzwee/node-csgo-web-tradebot/graphs/contributors)
---
+++ index.js
@@ -0,0 +1,214 @@
+'use strict'
+
+// Modules
+const express = require('express')
+
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const passport = require('passport')
+
+const session = require('express-session')
+const sharedsession = require('express-socket.io-session')
+const SteamStrategy = require('passport-steam').Strategy
+// Site stuff
+const TradeBot = require('./lib/index')
+
+const Trade = new TradeBot({ io })
+const config = require('./config')
+// Web server
+server.listen(config.websitePort)
+console.log('[!] Website server is online.')
+console.log('[!] Socket server is online.')
+// Passport
+passport.serializeUser((user, done) => {
+    done(null, user)
+})
+passport.deserializeUser((obj, done) => {
+    done(null, obj)
+})
+passport.use(new SteamStrategy({
+    returnURL: `${config.website}/auth/steam/return`,
+    realm: `${config.website}/`,
+    apiKey: config.steamApiKey,
+},
+(identifier, profile, done) => {
+    process.nextTick(() => {
+        const user = profile
+        user.identifier = identifier
+        return done(null, user)
+    })
+}))
+const sessionMiddleware = session({
+    secret: 'csg0tradebot',
+    name: 'csg0trade',
+    resave: true,
+    saveUninitialized: true,
+})
+app.use(sessionMiddleware)
+app.use(passport.initialize())
+app.use(passport.session())
+app.use('/static', express.static('./static'))
+// Routes
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/index.html`)
+})
+// Auth Routes
+app.get('/auth/steam', passport.authenticate('steam'))
+app.get('/auth/steam/return', passport.authenticate('steam', { failureRedirect: '/auth/steam' }), (req, res) => {
+    // Successful authentication, redirect home.
+    res.redirect('/')
+})
+app.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+})
+// Sockets
+io.use(sharedsession(sessionMiddleware))
+io.on('connection', (socket) => {
+    let userObject = false
+    if (
+        typeof socket.handshake.session.passport !== 'undefined' &&
+        typeof socket.handshake.session.passport.user !== 'undefined' &&
+        typeof socket.handshake.session.passport.user.id !== 'undefined'
+    ) {
+        userObject = socket.handshake.session.passport.user
+    }
+
+    socket.emit('site', config.site)
+    socket.emit('user', userObject)
+    socket.on('get user inv', (steamID64) => {
+        Trade.getInventory(steamID64, '730', '2', (err, data) => {
+            socket.emit('user inv', { error: err, items: data })
+        })
+    })
+    socket.on('get bot inv', (id) => {
+        Trade.getInventory(config.bots[id].steamID64, '730', '2', (err, data) => {
+            socket.emit('bot inv', { error: err, items: data })
+        })
+    })
+    socket.on('get bots inv', () => {
+        const params = []
+        Object.keys(config.bots).forEach((index) => {
+            const bot = config.bots[index]
+            params.push({
+                id: index,
+                steamID64: bot.steamID64,
+                appID: '730',
+                contextID: '2',
+            })
+        })
+        Trade.getInventories(params, (data) => {
+            socket.emit('bots inv', data)
+            socket.emit('bots floats', Trade.getFloatValues())
+        })
+    })
+    socket.on('get pricelist', () => {
+        socket.emit('pricelist', Trade.getPriceList())
+    })
+    socket.on('get rates', () => {
+        socket.emit('rates', {
+            ignore: Trade.getIgnorePrice(),
+            trash: Trade.getTrashPrice(),
+            user: Trade.getUserRates(),
+            bot: Trade.getBotRates(),
+        })
+    })
+    socket.on('get offer', (data) => {
+        socket.emit('offer status', {
+            error: null,
+            status: 4,
+        })
+        const link = data.tradelink
+        const offerData = data
+        if (
+            link.indexOf('steamcommunity.com/tradeoffer/new/') === -1 ||
+            link.indexOf('?partner=') === -1 ||
+            link.indexOf('&token=') === -1
+        ) {
+            socket.emit('offer status', {
+                error: 'Invalid trade link!',
+                status: false,
+            })
+        } else {
+            Trade.validateOffer(offerData, (err, success) => {
+                socket.emit('offer status', {
+                    error: err,
+                    status: (success) ? 1 : false,
+                })
+                if (!err && success) {
+                    if (typeof config.bots[offerData.bot_id] === 'undefined') {
+                        offerData.bot_id = Object.keys(config.bots)[0]
+                    }
+                    const Bot = Trade.getBot(offerData.bot_id)
+                    const offer = Bot.manager.createOffer(offerData.tradelink)
+                    offer.addTheirItems(offerData.user.map(assetid => ({
+                        assetid,
+                        appid: 730,
+                        contextid: 2,
+                        amount: 1,
+                    })))
+                    if (offerData.bot.length) {
+                        offer.addMyItems(offerData.bot.map(assetid => ({
+                            assetid,
+                            appid: 730,
+                            contextid: 2,
+                            amount: 1,
+                        })))
+                    }
+                    offer.setMessage(config.tradeMessage)
+                    offer.getUserDetails((detailsError, me, them) => {
+                        if (detailsError) {
+                            socket.emit('offer status', {
+                                error: detailsError,
+                                status: false,
+                            })
+                        } else if (me.escrowDays + them.escrowDays > 0) {
+                            socket.emit('offer status', {
+                                error: 'You must have 2FA enabled, we do not accept trades that go into Escrow.',
+                                status: false,
+                            })
+                        } else {
+                            offer.send((errSend, status) => {
+                                if (errSend) {
+                                    socket.emit('offer status', {
+                                        error: errSend,
+                                        status: false,
+                                    })
+                                } else {
+                                    console.log('[!!!!!] Sent a trade: ', data)
+                                    if (status === 'pending') {
+                                        socket.emit('offer status', {
+                                            error: null,
+                                            status: 2,
+                                        })
+                                        Trade.botConfirmation(data.bot_id, offer.id, (errConfirm) => {
+                                            if (!errConfirm) {
+                                                socket.emit('offer status', {
+                                                    error: null,
+                                                    status: 3,
+                                                    offer: offer.id,
+                                                })
+                                            } else {
+                                                socket.emit('offer status', {
+                                                    error: errConfirm,
+                                                    status: false,
+                                                })
+                                            }
+                                        })
+                                    } else {
+                                        socket.emit('offer status', {
+                                            error: null,
+                                            status: 3,
+                                            offer: offer.id,
+                                        })
+                                    }
+                                }
+                            })
+                        }
+                    })
+                }
+            })
+        }
+    })
+})
